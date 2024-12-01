15932e6209f5d6814dc737697f86e02b12b8626e
Mads R. Havmand <mads@v42.dk>
2024-12-01 23:17:59 +0100

    Revert "rename module"
    This reverts commit 6f04556e5a9fb2b8771886417a10108c295971c4.

--- example_test.go
+++ example_test.go
@@ -3,7 +3,7 @@ package configuration_secret_files_test
 import (
 	"fmt"
 	"github.com/BoRuDar/configuration/v4"
-	configuration_secret_files "github.com/MadsRC/configsilly"
+	configuration_secret_files "github.com/MadsRC/configuration-secret-files"
 	"os"
 	"path/filepath"
 )
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/MadsRC/configsilly
+module github.com/MadsRC/configuration-secret-files
 
 go 1.23
 
